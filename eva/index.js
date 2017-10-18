d67f50bef3d60ed0774c5cd8fd813ae485fce5b9
kaliumxyz <kalium@kalium.xyz>
2017-10-18 20:24:46 +0200

    [eva] can reply

---
+++ eva/index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const readline = require('readline');
+const config = require('.config.json')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+	// console.log(message)
+	if (message.channel.type === "dm" && !message.author.bot) {
+		// rl.question('would you like to reply?', x =>
+		// 	message.reply(x)
+		// );
+		message.reply("yes!");
+		console.log(message.content);
+	}
+});
+
+
+
+client.login(config.client);
