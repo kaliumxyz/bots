71f9821da6ec65d7e3fc331bff51aff74a306dc5
kaliumxyz <kalium@kalium.xyz>
2017-10-20 17:42:47 +0200

    [eva] add admin commands and logging

--- eva/index.js
+++ eva/index.js
@@ -1,28 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const readline = require('readline');
-const config = require('./.config.json')
+
+/* configurtation */
+const config = require('./.config.json') || { api: void(0) };
+
+/* logging */
+const logStream = fs.createWriteStream(path.join(__dirname, `application.log`), { flags: 'a' });
+const log = require('k-log')(logStream);
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 client.on('ready', () => {
-	console.log('I am ready!');
+	log('bot initiated');
 });
 
 client.on('message', message => {
-	// console.log(message)
+	// log anything posted
+	log(`user: ${message.author.username}, ID: ${message.author.id}`, message.cleanContent);
+	let msgTimeout;
+
 	if (message.channel.type === "dm" && !message.author.bot) {
-		// rl.question('would you like to reply?', x =>
-		// 	message.reply(x)
-		// );
-		message.reply("yes!");
-		console.log(message.content);
+		// rl.question('would you like to reply?', x => {
+		// 	message.reply(x);
+		// 	rl.close();
+		// 	clearTimeout(msgTimeout);
+		// });
+
+		// setTimeout(_ => {
+			// rl.close();
+			message.reply("yes!");
+		// }, 4000);
+
 	}
 });
 
+rl.on('line', line => {
+	switch(line){
+		case('status'):
+		console.log(client.status.toString());
+		break;
+		case('help'):
+		console.log(`
+			status - gets the status number.
+			users - logs the users the bot has access to at any given moment.
+			`);
+		break;
+		case('users'):
+		client.users.forEach(x => console.log(x));
+		break;
+	}
+})
 
 
 client.login(config.client);
