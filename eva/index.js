5ce61f038e2847ae3bf48b5dcaf1e9c128fe9553
kaliumxyz <martijn@kalium.xyz>
2017-10-20 15:02:13 +0000

    [config] change .config.json to ./.config.json

--- eva/index.js
+++ eva/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const readline = require('readline');
-const config = require('.config.json')
+const config = require('./.config.json')
 
 const rl = readline.createInterface({
   input: process.stdin,
