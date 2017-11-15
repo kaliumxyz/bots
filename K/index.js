6483573b63ce7a508be6800c06d3c50a6136909f
kalium <martijn@kalium.xyz>
2017-11-15 15:37:02 +0100

    feat(bot) add @K for euphoria.io

---
+++ K/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const MarkovChain = require('markovchain');
+const euphoriaConnection = require('euphoria-connection');
+
+/* configurtation */
+// this doesn't actually work for replacing the default, left in because some update will probably add it
+const config = require('./config.json') || { nick: "K", room: "test", dataset: "dataset.txt" };
+// reads the entire dataset into RAM (will crash if your ram is smaller then the dataset)
+const dataset = fs.readFileSync(config.dataset, 'utf-8');
+const markov = new MarkovChain(dataset);
+
+const connection = new euphoriaConnection(config.room, 1);
+
+/* logging */
+const logStream = fs.createWriteStream(path.join(__dirname, `application.log`), { flags: 'a' });
+const log = require('k-log')(logStream);
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: true
+});
+
+connection.on('send-event', message => {
+	// log anything posted
+	log(`user: ${message.data.sender.name}, ${message.data.sender.id}`, message.data.content);
+
+	if (/@K/.test(message.data.content))
+		connection.post(markov.end(Math.ceil(Math.random() * 100 % 40)).process(), message.data.id);
+});
+
+
+rl.on('line', line => {
+	if (/quit/.test(line))
+		process.exit();
+	rl.prompt();
+})
+
+connection.once('ready', () => {
+	connection.nick(config.nick)
+	log('bot initiated');
+	rl.prompt();
+})
---
+++ K/readme.md
@@ -0,0 +1,23 @@
+# K
+> markov bot replacement so that people leave me alone
+
+## install
+configure a config.json file containing the nick, room, and dataset (defaults are K, &xkcd, ./dataset.txt) in the following format:
+
+```json
+{
+	"nick": "K",
+	"room": "xkcd",
+	"dataset": "dataset.txt"
+}
+```
+A default dataset is not included and should be added.
+
+## usage
+It will reply with any @mentions to the nickname with a generated reply of 1 to 40 words.
+
+## tests
+No tests are implemented yet.
+
+## license
+MIT © [Kalium](https://kalium.xyz)
